b1d53b541eda723641d9aa1dd0cf967305e438a2
pfaithmtan <pfaithmtan@gmail.com>
2019-09-09 15:41:14 -0700

    Fix bug
    - Assign timeBetweenSteps
    - Call setPosition on this

--- src/dancer.js
+++ src/dancer.js
@@ -2,14 +2,14 @@
 var makeDancer = function(top, left, timeBetweenSteps) {
   this.top = top;
   this.left = left;
-  this.timeBetweenSteps;
+  this.timeBetweenSteps = timeBetweenSteps;
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
   this.step();
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
+  this.setPosition(top, left);
 };
 
 makeDancer.prototype.step = function() {
