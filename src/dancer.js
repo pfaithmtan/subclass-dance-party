bd8e36b21450d36f9355a9cc137e7bbfb97733c8
pfaithmtan <pfaithmtan@gmail.com>
2019-09-09 17:22:50 -0700

    Fixed bug on line 22
    - Edit second argument of setTimeout

--- src/dancer.js
+++ src/dancer.js
@@ -6,19 +6,23 @@ var makeDancer = function(top, left, timeBetweenSteps) {
 
   // use jQuery to create an HTML <span> tag
   this.$node = $('<span class="dancer"></span>');
-  this.step();
+
+  //this.step();
+
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  this.setPosition(top, left);
+
+  //this.setPosition(top, left);
+  console.log(this);
 };
 
 makeDancer.prototype.step = function() {
   // the basic dancer doesn't do anything interesting at all on each step,
   // it just schedules the next step
-  setTimeout(this.step, timeBetweenSteps);
+  setTimeout(this.step, this.timeBetweenSteps);
 };
 
-makeDance.prototype.setPosition = function(top, left) {
+makeDancer.prototype.setPosition = function(top, left) {
   // Use css top and left properties to position our <span> tag
   // where it belongs on the page. See http://api.jquery.com/css/
   var styleSettings = {
